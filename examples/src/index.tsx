ec54df655f4cdbd0e429aaa5871e93a3bba8ab52
zhuhuishao <zhuhuishao@itouchtv.cn>
2020-12-17 11:22:31 +0800

    fix autoplay bug

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -10,33 +10,34 @@ export default class App extends React.Component<any, any> {
     componentDidMount() {
         const video = document.getElementById('tmv')
         const tmv = new Tmv({ 
-            device: 'mobile',
-            type: 'hls', 
+            device: 'pc',
+            type: 'mp4',
+            autoPlay: true, 
             toNextVideo: () => { alert('下一个视频回调函数') },
-            definition: [
-                {
-                    name: 'fhd',
-                    cName: '超清',
-                    url:
-                        'http://video2-cloud.itouchtv.cn/video/2020/03/10/45dca3e86339ab421583827925794923__hd.mp4'
-                },
-                {
-                    name: 'hd',
-                    cName: '高清',
-                    url:
-                        'http://video2-cloud.itouchtv.cn/video/2020/03/18/687d9605547661c31584506645569577__hd.mp4'
-                },
-                {
-                    name: 'sd',
-                    cName: '标清',
-                    url:
-                        'http://video2-cloud.itouchtv.cn/video/2020/06/04/0cd6b2d3e3899e771591262568549928__hd.mp4'
-                }
-            ]
+            // definition: [
+            //     {
+            //         name: 'fhd',
+            //         cName: '超清',
+            //         url:
+            //             'http://video2-cloud.itouchtv.cn/video/2020/03/10/45dca3e86339ab421583827925794923__hd.mp4'
+            //     },
+            //     {
+            //         name: 'hd',
+            //         cName: '高清',
+            //         url:
+            //             'http://video2-cloud.itouchtv.cn/video/2020/03/18/687d9605547661c31584506645569577__hd.mp4'
+            //     },
+            //     {
+            //         name: 'sd',
+            //         cName: '标清',
+            //         url:
+            //             'http://video2-cloud.itouchtv.cn/video/2020/06/04/0cd6b2d3e3899e771591262568549928__hd.mp4'
+            //     }
+            // ]
             
         })
         tmv.attachMedia(video)
-        tmv.load()      
+        tmv.load()  
     }
 
     render() {
@@ -45,7 +46,7 @@ export default class App extends React.Component<any, any> {
                 <h1> JS Components </h1>
                 <div className="video-demo">
                     <h2>JS视频组件</h2>
-                    <video id="tmv" src="https://nclive.grtn.cn/jjkt/sd/live.m3u8?_upt=edcdc3b91608109201&auth_key=1608104015-0-0-df54e630a29b9133939a7013b0790c58" controls></video>
+                    <video id="tmv" src="http://video2-cloud.itouchtv.cn/video/2020/06/04/0cd6b2d3e3899e771591262568549928__hd.mp4"></video>
                 </div>
                 <div className="audio-demo">
                     <h2>JS音频组件</h2>
